20377bed623889e353110a270f90d80c13dcb9b7
Usuario <Usuario@CPD01-M02>
2024-03-28 15:54:40 -0300

    Adicionar arquivos de projeto.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,70 @@
+﻿namespace JmCalculadoraMobile
+{
+    public partial class MainPage : ContentPage
+    {
+        
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void maismenos_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Zerar_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void porcento_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dividir_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void num7_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void num8_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void num9_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void multiplicar_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void num4_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void num5_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void num6_Clicked(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
